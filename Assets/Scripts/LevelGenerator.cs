15b4464a2eb58d96d0d8b7a3053181dc41bd1470
Guillaume Gonthier <guillaume.gonthier@telecom-sudparis.eu>
2018-06-30 15:56:08 +0200

    :construction: C'est un début(tant :D)

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class LevelGenerator : MonoBehaviour {
+    private ArrayList gridPositions = new ArrayList(); //A list of possible locations to place tiles.
+
+    void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+}
