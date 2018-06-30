8451307d1dde688853f2ce072e8b24ec29d59e77
Guillaume Gonthier <guillaume.gonthier@telecom-sudparis.eu>
2018-06-30 18:49:40 +0200

    :sparkles: Following the player

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,22 +4,20 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
 
-    public GameObject Player;
-
 	void Start () {
 		
 	}
 
 	void Update () {
-        Vector3 PlayerPosition = Player.transform.position;
+        Vector3 PlayerPosition = transform.position;
         if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-            Player.transform.position = new Vector3(PlayerPosition.x - 1, PlayerPosition.y, PlayerPosition.z);
+            transform.position = new Vector3(PlayerPosition.x - 1, PlayerPosition.y, PlayerPosition.z);
         } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
-            Player.transform.position = new Vector3(PlayerPosition.x + 1, PlayerPosition.y, PlayerPosition.z);
+            transform.position = new Vector3(PlayerPosition.x + 1, PlayerPosition.y, PlayerPosition.z);
         } else if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            Player.transform.position = new Vector3(PlayerPosition.x, PlayerPosition.y, PlayerPosition.z + 1);
+            transform.position = new Vector3(PlayerPosition.x, PlayerPosition.y, PlayerPosition.z + 1);
         } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
-            Player.transform.position = new Vector3(PlayerPosition.x, PlayerPosition.y, PlayerPosition.z - 1);
+            transform.position = new Vector3(PlayerPosition.x, PlayerPosition.y, PlayerPosition.z - 1);
         }
     }
 }
