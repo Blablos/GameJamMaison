875e1fa5337f2ad307842df8154b55c07e2a78bd
Guillaume Gonthier <guillaume.gonthier@telecom-sudparis.eu>
2018-06-30 18:17:13 +0200

    :sparkles: Adding player movement

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -7,6 +7,8 @@ public class LevelGenerator : MonoBehaviour {
 
     private GameObject[,] BoardPositions;
     public GameObject CaseModel;
+    public int BoardLength = 5;
+    public int BoardWidth = 5;
 
     void InitBoard(int longueur, int largeur)
     {
@@ -16,7 +18,7 @@ public class LevelGenerator : MonoBehaviour {
             for (int j = 0; j < largeur; j++)
             {
                 var CaseInstance = Object.Instantiate(CaseModel,new Vector3(i,0, j), new Quaternion(),this.gameObject.transform);
-                Debug.Log("Bijour"+i+" et j :" +j );
+
                 if ((i+j)%2==0)
                 {
                     CaseInstance.GetComponentInChildren<Renderer>().material.color = Color.black;
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    public GameObject Player;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+        Vector3 PlayerPosition = Player.transform.position;
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            Player.transform.position = new Vector3(PlayerPosition.x - 1, PlayerPosition.y, PlayerPosition.z);
+        } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            Player.transform.position = new Vector3(PlayerPosition.x + 1, PlayerPosition.y, PlayerPosition.z);
+        } else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            Player.transform.position = new Vector3(PlayerPosition.x, PlayerPosition.y, PlayerPosition.z + 1);
+        } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            Player.transform.position = new Vector3(PlayerPosition.x, PlayerPosition.y, PlayerPosition.z - 1);
+        }
+    }
+}
